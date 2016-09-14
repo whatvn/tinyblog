30064e8dcfde3cfad2381b831085b17e3258f0ab
whatvn <hungnv@opensource.com.vn>
2016-09-14 19:03:43 +0700

    add hash function

---
+++ src/main/scala/com/pcdn/model/utils/Hash.scala
@@ -0,0 +1,11 @@
+package com.pcdn.model.utils
+
+import scala.util.hashing.MurmurHash3
+
+/**
+  * Created by Hung on 9/14/16.
+  */
+object Hash {
+  def toLong(x: String) = MurmurHash3.stringHash(x).toLong
+  def toHexString(x: String) = MurmurHash3.stringHash(x).toHexString
+}
