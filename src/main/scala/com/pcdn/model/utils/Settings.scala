20a5da14855c8358364d94a497b447a2cef88511
whatvn <hungnv@opensource.com.vn>
2016-12-12 17:56:58 +0700

    something is still remain from old spray routing

--- src/main/scala/com/pcdn/model/utils/Settings.scala
+++ src/main/scala/com/pcdn/model/utils/Settings.scala
@@ -6,8 +6,6 @@ package com.pcdn.model.utils
 
 import com.pcdn.model.TinyActor
 import com.typesafe.config.{Config, ConfigFactory}
-import spray.httpx.encoding.{Deflate, Gzip, NoEncoding}
-import spray.routing.directives.CompressResponseMagnet
 
 trait Settings {
   implicit val system = TinyActor.getSystem()
@@ -17,6 +15,4 @@ trait Settings {
   final val githubToken: String = configLoader.getString("blog.github.token")
   final val githubRepo: String = configLoader.getString("blog.github.repo")
   final val domainName: String = configLoader.getString("blog.domain")
-
-  def compressResponseMagnet: CompressResponseMagnet = CompressResponseMagnet.fromEncoders3((Gzip, Deflate, NoEncoding))
 }
